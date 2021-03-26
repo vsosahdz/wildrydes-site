53317805d398a93476c19e7cec4961acdb749684
vsosahdz <vsosa@tec.mx>
2021-03-26 00:49:24 -0600

    Configure API invokeURL

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     api: {
-        invokeUrl: '' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: 'https://rkm0zgyj9h.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
     }
 }
